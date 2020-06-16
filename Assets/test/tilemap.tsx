9185e9a9c5f7024470dc88c514bf69f0f2c3090c
204113 <sc_12340@hotmail.com>
2020-06-16 19:55:48 -0300

    lvl1 tiled

--- Assets/test/tilemap.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.3.5" name="tilemap" tilewidth="21" tileheight="21" spacing="2" margin="2" tilecount="841" columns="29">
- <image source="platformerart_pixelredux_2/Tilemap/tilemap.png" width="688" height="688"/>
-</tileset>
--- Sources/states/GameState.hx
+++ Sources/states/GameState.hx
@@ -1,22 +1,36 @@
 package states;
 
+import com.collision.platformer.Tilemap;
+import kha.Assets;
 import com.loading.basicResources.JoinAtlas;
+import com.loading.basicResources.DataLoader;
 import kha.input.KeyCode;
 import com.framework.utils.Input;
 import com.loading.Resources;
 import com.framework.utils.State;
 
 class GameState extends State {
+	var worldMap:Tilemap;
+	var simulationLayer:Layer;
 
 
 	override function load(resources:Resources) {
+		resources.add(new DataLoader(Assets.blobs.lvl1_tmxName));
 		var atlas:JoinAtlas = new JoinAtlas(2048, 2048);
 	}
 
-	override function init() {}
+	override function init() {
+		worldMap = new Tilemap("lvl1_tmx", 1);
+		worldMap.init(function(layerTilemap, tileLayer) {
+			if (!tileLayer.properties.exists("noCollision")) {
+				layerTilemap.createCollisions(tileLayer);
+			}
+			simulationLayer.addChild(layerTilemap.createDisplay(tileLayer, new Sprite("tiles2")));
+		}, parseMapObjects);
+	}
 
 	override function update(dt:Float) {
-        super.update(dt);
+		super.update(dt);
 		reset();
 	}
 
